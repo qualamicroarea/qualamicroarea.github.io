36eadf74c321f033b003233b6343d1623cfc08bc
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-29 20:46:05 -0300

    Working search.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
-var CURRENT_FOCUS = -1;
-
 var DATABASE = null;
-var POSSIVEIS_RUAS = [];
 
 var CIDADE_ATUAL = ""
 
@@ -10,163 +7,117 @@ var UNIDADE_ATUAL = ""
 
 
 
-function LoadScript(url, callback) {
-    var new_id = "loadscript_" + url
-
-    if (!document.getElementById(new_id)) {
-        var head = document.head;
-
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.id = new_id;
-        script.src = url;
-    
-        script.onreadystatechange = callback;
-        script.onload = callback;
-    
-        head.appendChild(script);
+function RemoveIfExists(elem) {
+    if (elem) {
+        elem.parentNode.removeChild(elem);
     }
 }
 
 
-function CheckCidadeUnidade(callback) {
-    var cidade_sel = document.getElementById("cb_cidade").value;
-    var unidade_sel = document.getElementById("cb_unidade").value;
+
+function GetRuasDict() {
+    if (DATABASE) {
+        const ruas = DATABASE["ruas"];
+        if (ruas) {
+            return ruas;
+        }
+    }
+    return null;
+}
+
+
+
+function LoadScript(url, callback) {
+    const new_id = "loadscript_" + url;
+
+    var script_tag = document.getElementById(new_id);
+    RemoveIfExists(script_tag);
+
+    var new_script = document.createElement("script");
+    new_script.type = "text/javascript";
+    new_script.id = new_id;
+    new_script.src = url;
+
+    new_script.onreadystatechange = callback;
+    new_script.onload = callback;
+
+    document.head.appendChild(new_script);
+}
+
+
+
+function CheckCidadeUnidade() {
+    const cidade_sel = document.getElementById("cb_cidade").value;
+    const unidade_sel = document.getElementById("cb_unidade").value;
+    const datalist_ruas = document.getElementById("datalist_ruas");
 
     if (cidade_sel !== CIDADE_ATUAL || unidade_sel !== UNIDADE_ATUAL) {
         CIDADE_ATUAL = cidade_sel;
         UNIDADE_ATUAL = unidade_sel;
-        LoadScript("database/" + cidade_sel + ".js", function() {
-            if (DATABASE) {
-                var unidade = DATABASE[UNIDADE_ATUAL];
-                if (unidade) {
-                    var ruas = unidade["ruas"];
-                    if (ruas) {
-                        POSSIVEIS_RUAS = ruas;
-                    }
-                }
-            }
 
-            callback();
+        const database_name = "database/" + cidade_sel + "/" + unidade_sel + ".js"
+
+        LoadScript(database_name, function() {
+            const ruas = GetRuasDict();
+            if (ruas) {
+                Object.keys(ruas).forEach(function(rua) {
+                    var option = document.createElement("option");
+                    option.value = rua;
+
+                    datalist_ruas.appendChild(option);
+                });
+            }
         });
-    } else {
-        callback();
     }
 }
 
 
 
-function CompletarRua_OnInput(keys) {
-    var elem = keys.target;
-    var text = elem.value;
-
-
-    CheckCidadeUnidade(function() {
-        CloseAllLists();
-
-        if (text) {
-            CURRENT_FOCUS = -1;
-    
-            var div = document.createElement("DIV");
-            div.setAttribute("id", elem.id + "autocomplete-list");
-            div.setAttribute("class", "autocomplete-items");
-    
-            elem.parentNode.appendChild(div);
-    
-            for (i = 0; i < POSSIVEIS_RUAS.length; i++) {
-                var text_lenght = text.length;
-                var possivel_rua = POSSIVEIS_RUAS[i];
-    
-                if (possivel_rua.substr(0, text_lenght).toUpperCase() == text.toUpperCase()) {
-                    var subdiv = document.createElement("DIV");
-    
-                    subdiv.innerHTML = "<strong>" + possivel_rua.substr(0, text_lenght) + "</strong>";
-                    subdiv.innerHTML += possivel_rua.substr(text_lenght);
-                    subdiv.innerHTML += "<input type='hidden' value='" + possivel_rua + "'>";
-    
-                    subdiv.addEventListener("click", function() {
-                        elem.value = this.getElementsByTagName("input")[0].value;
-                        CloseAllLists();
-                    });
-    
-                    div.appendChild(subdiv);
-                }
-            }
-        }
-    });
+function ResetMicroarea() {
+    var microarea_div = document.getElementById("microarea_div");
+    RemoveIfExists(microarea_div);
 }
 
 
 
-function CompletarRua_OnKeyDown(keyinfo) {
-    var list = document.getElementById(this.id + "autocomplete-list");
-    if (list) {
-        items = list.getElementsByTagName("div");
+function MostrarMicroarea(rua, microarea) {
+    var div = document.createElement("div");
+    div.className = "microarea_result"
+    div.id = "microarea_div"
+    div.innerHTML = "A rua <contrast>" + rua + "</contrast> pertence à microárea <contrast>" + microarea + "</contrast>.";
+    
+    document.getElementById("microarea_body").appendChild(div);
+}
 
-        if (keyinfo.keyCode == 40) {  // Down
-            CURRENT_FOCUS++;
-            SetActive(items);
-        } else if (keyinfo.keyCode == 38) {  // Up
-            CURRENT_FOCUS--;
-            SetActive(items);
-        } else if (keyinfo.keyCode == 13) {  // Enter
-            keyinfo.preventDefault();
-            if (CURRENT_FOCUS > -1 && items) {
-                items[CURRENT_FOCUS].click();
-            }
+
+
+function VerificaMicroarea(rua) {
+    ResetMicroarea();
+
+    const ruas = GetRuasDict();
+    if (ruas && rua in ruas) {
+        const microarea = ruas[rua];
+        if (microarea) {
+            MostrarMicroarea(rua, microarea);
         }
     }
 }
 
 
 
-function CloseAllLists(elem) {
-    var items = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < items.length; i++) {
-        var item = items[i]
-        if (elem != item && item.id !== "tf_nomedarua") {
-            item.parentNode.removeChild(item);
-        }
-    }
+function OnRuaInput(keys) {
+    CheckCidadeUnidade();
+    VerificaMicroarea(keys.target.value);
 }
 
 
 
-function SetActive(elem) {
-    if (elem) {
-        ResetActive(elem);
-        if (CURRENT_FOCUS >= elem.length) {
-            CURRENT_FOCUS = 0;
-        }
-        if (CURRENT_FOCUS < 0) {
-            CURRENT_FOCUS = (elem.length - 1);
-        }
-        elem[CURRENT_FOCUS].classList.add("autocomplete-active");
-    }
-}
-
-
-
-function ResetActive(elem) {
-    for (var i = 0; i < elem.length; i++) {
-        elem[i].classList.remove("autocomplete-active");
-    }
-}
-
-
-
-function linkHandlers() {
-    var tf_nomedarua = document.getElementById("tf_nomedarua");
-    tf_nomedarua.oninput = CompletarRua_OnInput;
-    tf_nomedarua.onkeydown = CompletarRua_OnKeyDown;
-
-    document.addEventListener("click", function(elem) {
-        CloseAllLists(elem.target);
-    });
+function LinkHandlers() {
+    document.getElementById("tf_nomedarua").oninput = OnRuaInput;
 }
 
 
 
 window.onload = (function() {
-    linkHandlers();
+    LinkHandlers();
 });
