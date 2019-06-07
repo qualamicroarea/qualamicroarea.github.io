234d982c8b9526f141d2fdaa47ce0f20fc251c00
pr0crustes <dark.pr0crustes@gmail.com>
2019-06-07 18:30:51 -0300

    Rewoerked microareas.

--- microareas.js
+++ microareas.js
@@ -1,94 +1,107 @@
-var DATABASE_FORM = null;
-var DATABASE_MANAGER = null;
-
-
 
 /**
- * Function that builds and returns a HTML string with all the info for a given microarea and microareainfo.
- * @param {string} microarea the microarea name.
- * @param {dict} microareainfo the microarea info.
+ * Subclass of BasePage, contains all logic for the microareas page.
  */
-function HTMLForMicroarea(microarea, microareainfo) {
-    var html = [
-        "<p class=\"info_microarea_nome\">Microárea ", microarea, "</p>",
-        "<div", IfMobile("", " class=\"side_by_side_container\""), ">",
-            "<table class=\"width_50 border_table\">",
-                MergeTableInfo(microareainfo, "Água Encanada", "agua_encanada", true),
-                MergeTableInfo(microareainfo, "Luz Elétrica", "luz_eletrica", true),
-                MergeTableInfo(microareainfo, "Esgoto Encanado", "esgoto_encanado", true),
-                MergeTableInfo(microareainfo, "Pontos de Lazer", "lazer", true),
-                MergeTableInfo(microareainfo, "Pontos de Ônibus", "onibus_atende", true),
-                MergeTableInfo(microareainfo, "Animais de Rua", "animais_de_rua", false),
+class MicroareasPage extends BasePage {
 
-                MergeTableInfo(microareainfo, "Lixeiras", "lixeira", true),
-                MergeTableInfo(microareainfo, "Lixo na Rua", "lixo_na_rua", false),
+    /**
+     * Simple constructor that fills important stuff.
+     */
+    constructor() {
+        super(DATABASE, document.getElementById("databaseform_container"));
 
-                MergeTableInfo(microareainfo, "Igrejas", "igrejas", true),
-                MergeTableInfo(microareainfo, "Bares", "bares", true),
-            "</table>",
+        document.getElementById("cb_microarea").onchange = function(keys) {
+            this.checkMicroarea(keys.target.value);
+        }.bind(this);
+    }
 
-            "<div", IfMobile("", " class=\"flex_item width_50\""), ">",
-                "<div class=\"width_85 center\">",
-                    "<p>", microareainfo["observacoes"], "</p>",
+    /**
+     * Builds and returns a HTML string with all the info for a given microarea and microareainfo.
+     * @param {string} microarea the microarea name.
+     * @param {dict} microareainfo the microarea info.
+     */
+    HTMLForMicroarea(microarea, microareainfo) {
+        var html = [
+            "<p class=\"info_microarea_nome\">Microárea ", microarea, "</p>",
+            "<div", IfMobile("", " class=\"side_by_side_container\""), ">",
+                "<table class=\"width_50 border_table\">",
+                    MergeTableInfo(microareainfo, "Água Encanada", "agua_encanada", true),
+                    MergeTableInfo(microareainfo, "Luz Elétrica", "luz_eletrica", true),
+                    MergeTableInfo(microareainfo, "Esgoto Encanado", "esgoto_encanado", true),
+                    MergeTableInfo(microareainfo, "Pontos de Lazer", "lazer", true),
+                    MergeTableInfo(microareainfo, "Pontos de Ônibus", "onibus_atende", true),
+                    MergeTableInfo(microareainfo, "Animais de Rua", "animais_de_rua", false),
+
+                    MergeTableInfo(microareainfo, "Lixeiras", "lixeira", true),
+                    MergeTableInfo(microareainfo, "Lixo na Rua", "lixo_na_rua", false),
+
+                    MergeTableInfo(microareainfo, "Igrejas", "igrejas", true),
+                    MergeTableInfo(microareainfo, "Bares", "bares", true),
+                "</table>",
+
+                "<div", IfMobile("", " class=\"flex_item width_50\""), ">",
+                    "<div class=\"width_85 center\">",
+                        "<p>", microareainfo["observacoes"], "</p>",
+                    "</div>",
                 "</div>",
-            "</div>",
-        "</div>"
-    ].join("");
+            "</div>"
+        ].join("");
 
-    return html;
-}
-
-
-
-/**
- * Function that displays a microarea.
- * @param {string} microarea the microarea name.
- * @param {dict} microareainfo the microarea info.
- */
-function DisplayMicroarea(microarea, microareainfo) {
-    if (microareainfo) {
-        var div = document.createElement("div");
-        div.className = "microarea_result";
-        div.id = "microarea_div";
-        div.innerHTML = HTMLForMicroarea(microarea, microareainfo);
-
-        document.getElementById("microarea_body").appendChild(div);
+        return html;
     }
-}
 
+    /**
+     * Displays a microarea.
+     * @param {string} microarea the microarea name.
+     * @param {dict} microareainfo the microarea info.
+     */
+    displayMicroarea(microarea, microareainfo) {
+        if (microareainfo) {
+            var div = document.createElement("div");
+            div.className = "microarea_result";
+            div.id = "microarea_div";
+            div.innerHTML = this.HTMLForMicroarea(microarea, microareainfo);
 
-
-/**
- * Function that searches the database for a microarea, displaying it, if existent.
- * @param {string} microarea the microarea name.
- */
-function CheckMicroarea(microarea) {
-    RemoveIfExistsId("microarea_div");
-
-    const microareas = DATABASE_MANAGER.getMicroareasDict();
-    if (microareas && microarea in microareas) {
-        DisplayMicroarea(microarea, microareas[microarea]);
+            document.getElementById("microarea_body").appendChild(div);
+        }
     }
-}
 
+    /**
+     * Searches the database for a microarea, displaying it, if existent.
+     * @param {string} microarea the microarea name.
+     */
+    checkMicroarea(microarea) {
+        RemoveIfExistsId("microarea_div");
 
+        const microareas = this.database_manager.getMicroareasDict();
+        if (microareas && microarea in microareas) {
+            this.displayMicroarea(microarea, microareas[microarea]);
+        }
+    }
 
-/**
- * Function called when the window is loaded, used to link all needed handlers.
- */
-function OnWindowLoad() {
-    DATABASE_FORM = new DatabaseForm(DATABASE);
-    DATABASE_FORM.on_change_handle = function(cidade, unidade) {
-        DATABASE_MANAGER = DATABASE_FORM.getDatabaseManager();
+    /**
+     * Implementation of method called after page loaded, use to check params.
+     */
+    checkGetParams() {
+        const rua = GetURLParam("rua");
+        if (rua) {
+            document.getElementById("tf_nomedarua").value = rua;
+            this.checkRua(rua);
+        }
+    }
+
+    /**
+     * Overriding BasePage method.
+     * @param {*} cidade 
+     * @param {*} unidade 
+     */
+    onFormChangeHandle(cidade, unidade) {
+        super.onFormChangeHandle(cidade, unidade);
 
         const cb_microarea = document.getElementById("cb_microarea");
         RemoveChildren(cb_microarea);
 
-        cb_microarea.onchange = function(keys) {
-            CheckMicroarea(keys.target.value);
-        };
-
-        var microareas = DATABASE_MANAGER.getMicroareasDict();
+        var microareas = this.database_manager.getMicroareasDict();
         if (microareas) {
             microareas["0"] = null;
             Object.keys(microareas).forEach(function(microarea) {
@@ -98,19 +111,25 @@ function OnWindowLoad() {
 
                 cb_microarea.appendChild(option);
             });
-
-            const microarea_get = GetURLParam("microarea");
-            if (microareas[microarea_get]) {
-                cb_microarea.value = microarea_get;
-                CheckMicroarea(microarea_get);
-            }
         }
-    };
-    DATABASE_FORM.setupForm(document.getElementById("databaseform_container"));
+    }
 
-    LinkStaticButtons();
+    /**
+     * Implementation of check get params, called after page load.
+     */
+    checkGetParams() {
+        const microareas = this.database_manager.getMicroareasDict();
+        const microarea_get = GetURLParam("microarea");
+        if (microareas[microarea_get]) {
+            document.getElementById("cb_microarea").value = microarea_get;
+            this.checkMicroarea(microarea_get);
+        }
+    }
 }
 
 
 
-window.onload = OnWindowLoad;
+var page = null;
+window.onload = function() {
+    page = new MicroareasPage();
+};
